6a167ddbe1bb94593161d77220b5e85f34f6c18f
Ravish Arora <5879193+ravarora@users.noreply.github.com>
2023-08-22 14:39:46 +0200

    Styling text

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -27,7 +27,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,11 +47,12 @@ class MainActivity : ComponentActivity() {
                     //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ImageCard(
-                        title = "Title",
-                        painter = painterResource(id = R.drawable._249545),
-                        contentDesc = "Content Desc"
-                    )
+//                    ImageCard(
+//                        title = "Title",
+//                        painter = painterResource(id = R.drawable._249545),
+//                        contentDesc = "Content Desc"
+//                    )
+                    StyledText()
                 }
             }
         }
@@ -127,5 +132,37 @@ fun ImageCard(
             )
 
         }
+    }
+}
+
+@Composable
+fun StyledText(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp
+                )) {
+                    append("H")
+                }
+                append("ello")
+                withStyle(style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp
+                )) {
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth()
+        )
     }
 }
