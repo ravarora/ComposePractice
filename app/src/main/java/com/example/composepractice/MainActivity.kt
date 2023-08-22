4e1426d09c08bf1a090c9eb9c10fac223ab599ea
Ravish Arora <5879193+ravarora@users.noreply.github.com>
2023-08-22 16:02:48 +0200

    Using State and colors

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,6 +21,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -36,6 +41,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +58,8 @@ class MainActivity : ComponentActivity() {
 //                        painter = painterResource(id = R.drawable._249545),
 //                        contentDesc = "Content Desc"
 //                    )
-                    StyledText()
+                    //StyledText()
+                    ColorStateExample(Modifier.fillMaxSize())
                 }
             }
         }
@@ -144,17 +151,21 @@ fun StyledText(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = buildAnnotatedString {
-                withStyle(style = SpanStyle(
-                    color = Color.Green,
-                    fontSize = 50.sp
-                )) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
                     append("H")
                 }
                 append("ello")
-                withStyle(style = SpanStyle(
-                    color = Color.Green,
-                    fontSize = 50.sp
-                )) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
                     append("C")
                 }
                 append("ompose")
@@ -164,5 +175,48 @@ fun StyledText(modifier: Modifier = Modifier) {
             textAlign = TextAlign.Center,
             modifier = modifier.fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun ColorStateExample(modifier: Modifier = Modifier) {
+    var color by remember {
+        mutableStateOf(Color.Yellow)
+    }
+    Column(modifier) {
+        ColorBox(modifier = Modifier
+            .weight(1f)
+            .fillMaxSize()) {
+            color = it
+        }
+        Box(modifier = Modifier
+            .weight(1f)
+            .fillMaxSize()
+            .background(color)) {
+
+        }
+    }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            }
+    ) {
+
     }
 }
