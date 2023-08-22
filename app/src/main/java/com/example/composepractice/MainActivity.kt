ffb48f887a8b9929c9b71aa4039b07cbeab951c8
Ravish Arora <5879193+ravarora@users.noreply.github.com>
2023-08-22 12:58:44 +0200

    Understanding Modifiers

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.composepractice
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +31,7 @@ class MainActivity : ComponentActivity() {
             ComposePracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
@@ -31,10 +43,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth()
+            .border(5.dp, Color.Magenta)
+            .padding(5.dp)
+            .border(5.dp, Color.Blue)
+            .padding(5.dp)
+            .border(5.dp, Color.Red)
+            .padding(5.dp)
+    ) {
+        Text(text = "Hello")
+        Text(text = "World")
+    }
 }
 
 @Preview(showBackground = true)
